69b34df064cb6c4098e27529c97e12ef7009322d
Hoang Nhuoc Quy <hoangnhuocquy0@gmail.com>
2017-05-01 21:59:04 +0700

    ITO-000 update TestCaseRepository.java

--- src/main/java/onlinejudge/repository/TestCaseRepository.java
+++ src/main/java/onlinejudge/repository/TestCaseRepository.java
@@ -1,11 +1,9 @@
 package onlinejudge.repository;
 
-import java.math.BigInteger;
-
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import onlinejudge.domain.TestCase;
 
-public interface TestCaseRepository extends PagingAndSortingRepository<TestCase, BigInteger>{
+public interface TestCaseRepository extends PagingAndSortingRepository<TestCase, String>{
 
 }
