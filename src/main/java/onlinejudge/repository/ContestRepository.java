5642a127f6b0bab429798966b94eeef52534a3a8
Hoang Nhuoc Quy <hoangnhuocquy0@gmail.com>
2017-05-18 07:35:07 +0700

    ITO-000 get all team in which there is current user

--- src/main/java/onlinejudge/repository/ContestRepository.java
+++ src/main/java/onlinejudge/repository/ContestRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import onlinejudge.domain.Contest;
-import onlinejudge.domain.Problem;
 
 public interface ContestRepository extends PagingAndSortingRepository<Contest, String>{
 	public List<Contest> findListContestByAdminID(String adminID);
--- src/main/java/onlinejudge/repository/TeamRepository.java
+++ src/main/java/onlinejudge/repository/TeamRepository.java
@@ -1,11 +1,13 @@
 package onlinejudge.repository;
 
-import java.math.BigInteger;
+import java.util.List;
 
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import onlinejudge.domain.Team;
 
 public interface TeamRepository extends PagingAndSortingRepository<Team, String> {
-
+	@Query("{'listMember.email': ?0}")
+	List<Team> findTeamsByEmailOfMember(String emailOfMember);
 }
